df272525907d3c73b500814e1b113fd6f2024d75
sakhanov <sakhanov@yandex.ru>
2020-02-12 23:51:05 +0300

    Подключился к GOOGLE DISK не открывается фалй

--- GoogleConnect.py
+++ GoogleConnect.py
@@ -2,3 +2,12 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+key ="c:\KEY\investingmoex-cb91da954760.json"
+
+scope = ['https://spreadsheets.google.com/feeds']
+creds = ServiceAccountCredentials.from_json_keyfile_name(key, scope)
+client = gspread.authorize(creds)
+sheet = client.open("t1").sheet1
+
+list_of_hashes = sheet.get_all_records()
+print(list_of_hashes)
